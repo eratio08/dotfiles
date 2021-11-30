f42a7883c5e124e7479274ac5b00ec3ffd44f18c
Eike Lurz <eike.lurz@tuta.io>
2021-11-30 22:52:42 +0100

    ♻️ Add SchaDa settings

--- .config/nvim/lua/eratio/settings.lua
+++ .config/nvim/lua/eratio/settings.lua
@@ -59,3 +59,4 @@ opt.diffopt:append('vertical') -- diff windows split to vertical
 opt.completeopt = 'menu,menuone' -- show auto completion menu even for single item
 opt.splitright = true -- split to the right
 opt.foldenable = false -- unfold all by default
+opt.shada = { '!', "'1000", '<50', 's10' } -- set shared data saving, global upper case variables, 1000 marks, 50 lines per register, max 10KiB
