8c6504ce0da2a75af9465999786a873960369d23
Eike Lurz <eike.lurz@tuta.io>
2022-11-09 15:56:45 +0100

    Update alacritty settings

--- tmux-start.sh
+++ tmux-start.sh
@@ -6,7 +6,8 @@ tmux new-session -s $SESSION -d
 tmux rename-window "~" 
 tmux new-window -n "rpi" -c ~/src/pi-architecture/rpi4
 tmux new-window -n "nvim" -c ~/.config/nvim
-tmux new-window -n "rust" -c ~/src/rust
-tmux new-window -n "elm"  -c ~/src/
+tmux new-window -n "go" -c ~/src/go
+# tmux new-window -n "rust" -c ~/src/rust
+# tmux new-window -n "elm"  -c ~/src/elm
 
 tmux attach-session -t $SESSION
