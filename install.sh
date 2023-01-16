e5ab32c570e0cb758c7a98ea32926d7470c47af7
Eike Lurz <eike.lurz@tuta.io>
2023-01-16 12:43:52 +0100

    More stow related changes

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/zsh
 
-# use stow to 'install' dotfiles
-# see https://zsh.sourceforge.io/Doc/Release/Expansion.html#Glob-Qualifiers
-for file in .config/**/*(/); 
-  stow --dir .config --target ~/.config $file
+local SETUP = $0
+local STOWS
+case $SETUP in 
+  "manjaro")
+    STOWS=("nvim" "mako" "sway" "swaylock" "waybar" "tmux" "lvim" "manjaro")
+    ;;
+  "spa")
+    STOWS=("nvim" "tmux" "lvim" "spa")
+    ;;
+  "asahi")
+    STOWS=("nvim" "mako" "sway" "swaylock" "waybar" "tmux" "lvim" "asahi")
+    ;;
+  *)
+    print "Unknown setup"
+    exit 1
+  ;;
+esac
+
+for pkg in $STOWS; 
+  stow $pkg
