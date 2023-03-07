ae6033adbdcfcca3e8c228067d3d22831e368998
Eike Lurz <eike.lurz@tuta.io>
2023-03-07 21:59:58 +0100

    🩹 Fix typo in comment

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -2,8 +2,6 @@ vim.cmd('colorscheme rose-pine')
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })        -- make background transparent
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })   -- make background transparent
-
-vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })  -- hide window sparator
-
+vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })  -- hide window separator
 vim.api.nvim_set_hl(0, "NonText", { fg = "#292929" })    -- set color of listings
 vim.api.nvim_set_hl(0, "Whitespace", { fg = "#292929" }) -- set color of listings
