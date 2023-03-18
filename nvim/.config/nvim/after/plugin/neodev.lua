fc47cab63d772453c8e82d75a942cac8e5e7c020
Eike Lurz <eike.lurz@tuta.io>
2023-03-18 19:01:58 +0100

    Fix neodev setup

--- nvim/.config/nvim/after/plugin/neodev.lua
+++ nvim/.config/nvim/after/plugin/neodev.lua
@@ -1,3 +1,16 @@
 local neodev = require("neodev")
 
 neodev.setup()
+
+local lspconfig = require('lspconfig')
+
+-- example to setup lua_ls and enable call snippets
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace"
+      }
+    }
+  }
+})
