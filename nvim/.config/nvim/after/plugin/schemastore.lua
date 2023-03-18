109ca4e86131eaefba74685f62999f32f5b47913
Eike Lurz <eike.lurz@tuta.io>
2023-03-18 19:21:14 +0100

    Configure schemastore

---
+++ nvim/.config/nvim/after/plugin/schemastore.lua
@@ -0,0 +1,18 @@
+local lspconfig = require('lspconfig')
+
+lspconfig.jsonls.setup {
+  settings = {
+    json = {
+      schemas = require('schemastore').json.schemas(),
+      validate = { enable = true },
+    },
+  },
+}
+
+lspconfig.yamlls.setup {
+  settings = {
+    yaml = {
+      schemas = require('schemastore').yaml.schemas(),
+    },
+  },
+}
