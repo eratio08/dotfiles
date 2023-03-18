0f4d9864d8a4075c0e222d4a4c253e5119b7f5a6
Eike Lurz <eike.lurz@tuta.io>
2023-03-18 19:04:15 +0100

    Set up lsp fidget

---
+++ nvim/.config/nvim/after/plugin/fidget.lua
@@ -0,0 +1,21 @@
+local fidget = require("fidget")
+
+fidget.setup({
+  text = {
+    spinner = 'moon',
+    done = "✔",
+    commended = "🤖",
+    completed = "✔",
+  },
+  align = {
+    bottom = false,
+    right = true,
+  },
+  window = {
+    relative = "editor",
+    blend = "90"
+  },
+  fmt = {
+    leftpad = true,
+  },
+})
