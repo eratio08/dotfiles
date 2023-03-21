7e6bf8b510d7f420a888d12e270074c3a5125849
Eike Lurz <eike.lurz@tuta.io>
2023-03-21 21:31:30 +0100

    Adjust trouble key bindings

--- nvim/.config/nvim/after/plugin/trouble.lua
+++ nvim/.config/nvim/after/plugin/trouble.lua
@@ -4,13 +4,15 @@ trouble.setup()
 
 local wk = require('which-key')
 wk.register({
-  x = {
-    name = 'Trouble',
-    x = { ':TroubleToggle<CR>', 'Toggle' },
-    w = { ':TroubleToggle workspace_diagnostics<CR>', 'Workspace Diagnostics' },
-    d = { ':TroubleToggle document_diagnostics<CR>', 'Document Diagnostics' },
-    q = { ':TroubleToggle quickfix<CR>', 'Quickfix' },
-    l = { ':TroubleToggle loclist<CR>', 'LOC' },
-    r = { ':TroubleToggle lsp_references<CR>', 'LSP References' }
+  ['<leader>'] = {
+    t = {
+      name = 'Trouble',
+      t = { ':TroubleToggle<CR>', 'Toggle' },
+      w = { ':TroubleToggle workspace_diagnostics<CR>', 'Workspace Diagnostics' },
+      d = { ':TroubleToggle document_diagnostics<CR>', 'Document Diagnostics' },
+      q = { ':TroubleToggle quickfix<CR>', 'Quickfix' },
+      l = { ':TroubleToggle loclist<CR>', 'LOC' },
+      r = { ':TroubleToggle lsp_references<CR>', 'LSP References' }
+    }
   }
 })
