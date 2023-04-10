219579e677a0133cc03f09d8bd6b930fae116ccf
Eike Lurz <eike.lurz@tuta.io>
2023-04-10 11:49:56 +0200

    Change neotree git symbols

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -3,12 +3,37 @@ local nvim_tree = require('nvim-tree')
 nvim_tree.setup({
   disable_netrw = false,
   hijack_netrw = false,
+  filters = {
+    custom = {
+      '^.git$' }
+  },
   git = {
     ignore = false,
   },
   renderer = {
     highlight_git = true,
-  }
+    icons = {
+      git_placement = 'after',
+      show = {
+        git = true,
+      },
+      glyphs = {
+        git = {
+          unstaged = 'M',
+          staged = 'S',
+          unmerged = '',
+          renamed = 'R',
+          untracked = 'U',
+          deleted = 'D',
+          ignored = '',
+        }
+      },
+    },
+  },
+  hijack_directories = {
+    enable = false,
+    auto_open = false,
+  },
 })
 
 vim.api.nvim_set_hl(0, 'NvimTreeFileIgnored', {
