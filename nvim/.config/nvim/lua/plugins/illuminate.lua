f5e7434d2d7cafab8ad694cb1499cd823bc017c3
Eike Lurz <eike.lurz@tuta.io>
2023-08-13 14:01:05 +0200

    ♻️ Tweak illuminate setup
    Use bg highlighting and set min count to 2 word.

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -1,6 +1,16 @@
 return {
   'RRethy/vim-illuminate',
+  dependencies = {
+    { 'rose-pine/neovim' },
+  },
   config = function ()
-    require('illuminate').configure()
+    require('illuminate').configure({
+      min_count_to_highlight = 2,
+    })
+
+    local p = require('rose-pine.palette')
+    vim.api.nvim_set_hl(0, 'IlluminatedWordText', { bg = p.highlight_med })
+    vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { bg = p.highlight_med })
+    vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { bg = p.highlight_med })
   end
 }
