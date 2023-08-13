50c374f5777c32485e923546830beb88f20e6af4
Eike Lurz <eike.lurz@tuta.io>
2023-08-13 13:33:02 +0200

    ♻️ Move md preview key maps

--- nvim/.config/nvim/lua/plugins/md-preview.lua
+++ nvim/.config/nvim/lua/plugins/md-preview.lua
@@ -1,7 +1,20 @@
 return {
   'iamcco/markdown-preview.nvim',
-  lazy = true,
+  dependencies = {
+    { 'folke/which-key.nvim' }
+  },
   build = function ()
     vim.fn['mkdp#util#install']()
   end,
+  config = function ()
+    local wk = require('which-key')
+    wk.register({
+      ['<leader>'] = {
+        m = {
+          v = { ':MarkdownPreviewToggle<CR>', 'Markdown Preview' },
+          s = { ':MarkdownPreviewStop<CR>', 'Markdown Stop Preview' },
+        },
+      },
+    })
+  end
 }
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -16,10 +16,6 @@ return {
         },
         s = { '1z=', 'Fix spelling' },
         q = { vim.diagnostic.setloclist, 'Populate Quick fix list with diagnostics' },
-        m = {
-          v = { ':MarkdownPreviewToggle<CR>', 'Markdown Preview' },
-          s = { ':MarkdownPreviewStop<CR>', 'Markdown Stop Preview' },
-        }
       },
       ['<A-k>'] = { 'ddp', 'Move Line Up' },
       ['<A-j>'] = { 'ddkP', 'Move Line Down' },
