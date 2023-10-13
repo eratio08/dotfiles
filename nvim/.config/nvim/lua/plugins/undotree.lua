c8198bdc0d15b486af2de19af35dc188897ef05d
Eike Lurz <eike.lurz@sportalliance.com>
2023-10-13 07:36:32 +0200

    Add missing dependency to undotree

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,6 +1,9 @@
 return {
   'mbbill/undotree',
   keys = { '<leader>u' },
+  dependencies = {
+    { 'folke/which-key.nvim' }
+  },
   config = function ()
     local wk = require('which-key')
     wk.register({
