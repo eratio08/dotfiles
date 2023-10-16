e3964b66ef69d11b60ebbaae4054293e13e0aa80
Eike Lurz <eike.lurz@sportalliance.com>
2023-10-16 13:45:44 +0200

    Add dependabot helper

---
+++ spa/gh-dependebot.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+repos=(
+  "sportalliance/fi-perdix" 
+  "sportalliance/setup-mvnd-action"
+  "sportalliance/spa-download-workflow-artifacts-action"
+  "sportalliance/spa-typescript-action-template"
+  "sportalliance/setup-gh-cli-action"
+  "sportalliance/spa-datadog-downtime-action"
+  "sportalliance/fc-jira-release-action"
+)
+
+approve_prs() {
+  # Set repository as script argument
+  local repository=$1
+
+  # If no repository is supplied, exit with error message
+  if [ -z "$repository" ]; then
+    echo "Error: No repository provided. Please provide a repository as an argument in the format 'owner/repo'."
+    exit 1
+  fi
+
+  echo "Fetching pull requests from $repository by dependabot..."
+
+  # Fetch all pull requests from dependabot that are not already approved
+  prs=$(gh pr list -R $repository --json number,author,reviewDecision -q '.[] | select(.author.login == "app/dependabot" and .author.is_bot == true and .reviewDecision != "APPROVED") | .number')
+
+  # Check if PRs by dependabot were found
+  if [ -z "$prs" ]; then
+    echo "No unapproved pull requests by dependabot found."
+  else
+    echo "Found the following unapproved PR numbers by dependabot:"
+    echo "$prs"
+
+    # Loop through all the PRs
+    for pr in $prs
+    do
+      echo "Approving PR #$pr..."
+      # Approve the PR with the review comment
+      gh pr review "$pr" --approve --body "@dependabot squash and merge" -R $repository
+      echo "PR #$pr approved with comment."
+    done
+
+    echo "All unapproved dependabot PRs now approved."
+  fi 
+}
+
+for i in "${repos[@]}"
+do
+  echo "Inspecting $i..."
+  approve_prs $i
+  echo ""
+done
