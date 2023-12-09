39fbc1ba79c481752385bef94471efeca293116a
Eike Lurz <eike.lurz@tuta.io>
2023-12-09 23:46:21 +0100

    🐛 Fix usage of deprecated lua api

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -86,7 +86,7 @@ return {
       }, { buffer = bufnr })
 
       -- conditional bindings
-      local file_type = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+      local file_type = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
       if file_type == 'go' then
         wk.register {
           ['<leader>'] = {
