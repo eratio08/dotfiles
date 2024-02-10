f4cbb90a07de19b39db132631882f870692b52d7
Eike Lurz <eike.lurz@tuta.io>
2024-02-10 13:22:24 +0100

    Add ollama integration

---
+++ nvim/.config/nvim/lua/plugins/ollama-nvim.lua
@@ -0,0 +1,29 @@
+return {
+  'nomnivore/ollama.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  cmd = { 'Ollama', 'OllamaModel', 'OllamaServe', 'OllamaServeStop' },
+  keys = {
+    -- Note that the <c-u> is important for selections to work properly.
+    {
+      '<leader>oo',
+      ":<c-u>lua require('ollama').prompt()<cr>",
+      desc = 'ollama prompt',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>og',
+      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+      desc = 'ollama Generate Code',
+      mode = { 'n', 'v' },
+    },
+  },
+
+  ---@type Ollama.Config
+  opts = {
+    prompts = {
+      -- todo add general prompt
+    }
+  }
+}
