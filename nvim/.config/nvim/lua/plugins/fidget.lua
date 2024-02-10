8442dd98562c2462e21468b4e92a029ed6c29fd0
Eike Lurz <eike.lurz@tuta.io>
2024-02-10 13:22:14 +0100

    Fix coloring of fidget

--- nvim/.config/nvim/lua/plugins/fidget.lua
+++ nvim/.config/nvim/lua/plugins/fidget.lua
@@ -16,8 +16,15 @@ return {
       },
       window = {
         relative = 'editor',
-        blend = '90'
+        blend = 0,
+      },
+      fmt = {
+        stack_upwards = false,
       },
     })
+
+    local p = require('rose-pine.palette')
+    vim.api.nvim_set_hl(0, 'FidgetTitle', { bg = 'NONE', ctermbg = 'NONE', fg = p.foam })
+    vim.api.nvim_set_hl(0, 'FidgetTask', { bg = 'NONE', ctermbg = 'NONE', fg = p.pine })
   end
 }
