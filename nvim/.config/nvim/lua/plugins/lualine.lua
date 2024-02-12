38f874da7ef9a4ea8ae5874ef03180265d430a3e
Eike Lurz <eike.lurz@tuta.io>
2024-02-12 12:52:55 +0100

    Add ollama status to lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,22 @@ return {
   config = function ()
     local lualine = require('lualine')
 
+    -- Define a function to check the status and return the corresponding icon
+    local function ollama_status()
+      local ok, ollama = pcall(require, 'ollama')
+      if not ok then
+        return '❌'
+      end
+
+      local status = require('ollama').status()
+
+      if status == 'IDLE' then
+        return ''
+      elseif status == 'WORKING' then
+        return '🦙'
+      end
+    end
+
     lualine.setup({
       options = {
         icons_enabled = true,
@@ -18,7 +34,7 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'nvim_diagnostic' } } },
         lualine_c = { { 'filename', path = 1 } },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = { ollama_status, 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
       },
