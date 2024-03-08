5fc290e12646bd5fe7eb9a766356f7f83c6f5400
Eike Lurz <eike.lurz@tuta.io>
2024-03-08 18:04:32 +0100

    Improve ufo setup
    Improve fold column display.
    Enable highlighting and fold peaking.

--- nvim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -1,39 +1,74 @@
 return {
   'kevinhwang91/nvim-ufo',
-  event = 'BufEnter',
-  dependencies = { 'kevinhwang91/promise-async', 'nvim-treesitter/nvim-treesitter' },
+  event = 'VeryLazy',
+  dependencies = {
+    'kevinhwang91/promise-async',
+    'nvim-treesitter/nvim-treesitter',
+    { -- required to hide fold count number in fold column
+      'luukvbaal/statuscol.nvim',
+      config = function ()
+        local builtin = require('statuscol.builtin')
+        require('statuscol').setup({
+          relculright = true,
+          segments = {
+            { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
+            { text = { '%s' }, click = 'v:lua.ScSa' },
+            { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
+          },
+        })
+      end,
+    },
+  },
   config = function ()
     --- @diagnostic disable: unused-local
     local ufo = require('ufo')
-
-    -- lsp->treesitter->indent
-    local function customizeSelector(bufnr)
-      local function handleFallbackException(err, providerName)
-        if type(err) == 'string' and err:match('UfoFallbackException') then
-          return ufo.getFolds(providerName, bufnr)
-        else
-          return require('promise').reject(err)
-        end
-      end
-
-      return ufo.getFolds('lsp', bufnr):catch(function (err)
-        return handleFallbackException(err, 'treesitter')
-      end):catch(function (err)
-        return handleFallbackException(err, 'indent')
-      end)
-    end
-
     ufo.setup({
-      provider_selector = function (bufnr, filetype, buftype)
-        return customizeSelector
-      end
+      provider_selector = function ()
+        return function (bufnr)
+          -- providers: lsp -> treesitter -> indent
+          local function handleFallbackException(err, providerName)
+            if type(err) == 'string' and err:match('UfoFallbackException') then
+              return ufo.getFolds(providerName, bufnr)
+            else
+              return require('promise').reject(err)
+            end
+          end
+
+          return ufo.getFolds('lsp', bufnr):catch(function (err)
+            return handleFallbackException(err, 'treesitter')
+          end):catch(function (err)
+            return handleFallbackException(err, 'indent')
+          end)
+        end
+      end,
+      fold_virt_text_handler = function (virtText)
+        print(vim.inspect(virtText))
+        return virtText
+      end,
+      preview = {
+        win_config = {
+          border = { '', '─', '', '', '', '─', '', '' },
+          winhighlight = 'Normal:Folded',
+          winblend = 0,
+        },
+        mappings = {
+          scrollU = '<C-u>',
+          scrollD = '<C-d>',
+          jumpTop = '[',
+          jumpBot = ']',
+        },
+      },
     })
+
+    local p = require('rose-pine.palette')
+    vim.api.nvim_set_hl(0, 'Folded', { bg = p.highlight_low })
 
     require('which-key').register({
       z = {
         title = 'Fold',
         R = { ufo.openAllFolds, 'Open all' },
         M = { ufo.closeAllFolds, 'Close all' },
+        p = { ufo.peekFoldedLinesUnderCursor, 'Peek' }
       }
     })
   end
