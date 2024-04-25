ac73b13648fda9ed03eaed3c016e488b1fd8d836
Eike Lurz <eike.lurz@sportalliance.com>
2024-04-25 14:54:51 +0200

    Add yank highlighting

--- nvim/.config/nvim/lua/eRatio/cmd.lua
+++ nvim/.config/nvim/lua/eRatio/cmd.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_augroup('term_cmd', { clear = true })
 vim.api.nvim_create_autocmd({ 'TermOpen' }, {
-  group = 'term_cmd',
+  desc = 'Changes styling of terminal buffers.',
+  group = vim.api.nvim_create_augroup('term_cmd', { clear = true }),
   callback = function ()
     vim.wo.relativenumber = false
     vim.wo.number = false
@@ -8,4 +8,12 @@ vim.api.nvim_create_autocmd({ 'TermOpen' }, {
     vim.wo.spell = false
     vim.cmd(':startinsert')
   end
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function ()
+    vim.highlight.on_yank()
+  end,
 })
