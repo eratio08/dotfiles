cee1a6709961251335a8f084594a727e2bd074c8
Eike Lurz <eike.lurz@sportalliance.com>
2024-05-19 13:13:58 +0200

    Improve rainbow-delimiters performance

--- nvim/.config/nvim/lua/plugins/rainbow-delimiters.lua
+++ nvim/.config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -2,12 +2,21 @@ return {
   'HiPhish/rainbow-delimiters.nvim',
   event = 'VeryLazy',
   config = function ()
-    local rainbow_delimiters = require('rainbow-delimiters')
+    local rainbow = require('rainbow-delimiters')
     require('rainbow-delimiters.setup')(
       {
         strategy = {
-          [''] = rainbow_delimiters.strategy['global'],
-          commonlisp = rainbow_delimiters.strategy['local'],
+          [''] = rainbow.strategy['global'],
+          commonlisp = rainbow.strategy['local'],
+          go = function (bufnr)
+            local line_count = vim.api.nvim_buf_line_count(bufnr)
+            if line_count > 5000 then
+              return nil
+            elseif line_count > 1000 then
+              return rainbow.strategy['global']
+            end
+            return rainbow.strategy['local']
+          end
         },
         query = {
           [''] = 'rainbow-delimiters',
