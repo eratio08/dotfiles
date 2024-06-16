00f4a53a0801b4b31b8e85e78780591663db22f8
Eike Lurz <eike.lurz@tuta.io>
2024-06-16 12:18:59 +0200

    LLM-LS still does not work well

--- nvim/.config/nvim/lua/plugins/llm-nvim.lua
+++ nvim/.config/nvim/lua/plugins/llm-nvim.lua
@@ -1,29 +1,23 @@
 return {
+  'huggingface/llm.nvim',
   -- does not work well on m1 macbook
   enabled = false,
-  'huggingface/llm.nvim',
-  -- build = ':MasonInstall llm-ls',
+  event = 'VeryLazy',
   config = function ()
     require('llm').setup({
       backend = 'ollama',
       url = 'http://localhost:11434/api/generate',
-      model = 'codellama:7b',
-      request_body = {},
-      tokens_to_clear = { '<EOT>' },
+      model = 'codestral:latest',
       fim = {
         enabled = true,
-        prefix = '<PRE> ',
-        middle = ' <MID>',
-        suffix = ' <SUF>',
+        model = 'model',
+        prompt = 'prompt',
+        suffix = 'suffix',
       },
-      context_window = 4096,
-      tokenizer = {
-        repository = 'codellama/CodeLlama-7b-hf',
-      },
-      accept_keymap = '<c-g>',
-      dismiss_keymap = '<c-x>',
+      accept_keymap = '<c-j>',
+      -- dismiss_keymap = '<c-x>',
       lsp = {
-        -- bc llm-ls is managed with Mason
+        -- llm-ls is managed with Mason
         bin_path = vim.api.nvim_call_function('stdpath', { 'data' }) .. '/mason/bin/llm-ls',
       },
     })
