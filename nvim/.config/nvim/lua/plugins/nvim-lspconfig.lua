1482029b905c81c47ade37b5bf3d003ddd7724e4
Eike Lurz <eike.lurz@sportalliance.com>
2024-10-14 13:37:38 +0200

    🐛 Fix UFO lsp setup

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -170,6 +170,10 @@ return {
     }
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true
+    }
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
     require('mason-lspconfig').setup({
       handlers = {
