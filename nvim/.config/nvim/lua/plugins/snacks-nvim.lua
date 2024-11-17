a458024c0a119eebfa9c15227c414c429a347994
Eike Lurz <eike.lurz@tuta.io>
2024-11-17 20:50:02 +0100

    Add nacks-nvim
    Mainly for lazygit and notifications

---
+++ nvim/.config/nvim/lua/plugins/snacks-nvim.lua
@@ -0,0 +1,22 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    bufdelete = { enabled = false },
+    debug = { enabled = true },
+    git = { enabled = false },
+    gitbrowse = { enabled = true },
+    lazygit = { enabled = true },
+    notify = { enabled = false },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    rename = { enabled = false },
+    statuscolumn = { enabled = false },
+    terminal = { enabled = false },
+    toggle = { enabled = false },
+    win = { enabled = false },
+    words = { enabled = false },
+  },
+}
