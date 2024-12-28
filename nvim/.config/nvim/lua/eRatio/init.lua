5caf420b2e1d4a43e63c04de811f7ce53790bc7a
Eike Lurz <eike.lurz@sportalliance.com>
2024-12-28 15:09:26 +0100

    Improve ghostty tab names if nvim is opened

--- nvim/.config/nvim/lua/eRatio/init.lua
+++ nvim/.config/nvim/lua/eRatio/init.lua
@@ -2,3 +2,8 @@ require('eRatio.remap')
 require('eRatio.settings')
 require('eRatio.lazy')
 require('eRatio.cmd')
+
+if vim.fn.getenv('TERM_PROGRAM') == 'ghostty' then
+  vim.opt.title = true
+  vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
+end
