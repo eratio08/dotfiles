4fc470ad6bbd77657585ec024650faaf3fd29090
Eike Lurz <eike.lurz@tuta.io>
2025-02-06 20:52:17 +0100

    nvim: try tailwind lsp

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -155,6 +155,9 @@ return {
         filetypes = { 'elixir', 'eelixir', 'heex' },
         settings = {}
       },
+      tailwindcss = {
+        filetypes = { 'heex', 'aspnetcorerazor', 'astro', 'astro-markdown', 'blade', 'clojure', 'django-html', 'htmldjango', 'edge', 'eelixir', 'elixir', 'ejs', 'erb', 'eruby', 'gohtml', 'gohtmltmpl', 'haml', 'handlebars', 'hbs', 'html', 'htmlangular', 'html-eex', 'heex', 'jade', 'leaf', 'liquid', 'markdown', 'mdx', 'mustache', 'njk', 'nunjucks', 'php', 'razor', 'slim', 'twig', 'css', 'less', 'postcss', 'sass', 'scss', 'stylus', 'sugarss', 'javascript', 'javascriptreact', 'reason', 'rescript', 'typescript', 'typescriptreact', 'vue', 'svelte', 'templ' }
+      },
       roc_ls = {},
       helmls = {
         settings = {
