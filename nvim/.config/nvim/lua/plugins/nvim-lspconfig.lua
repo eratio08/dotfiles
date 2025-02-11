66097673104e27ca0eb5d1041b8fea62e7722b1a
Eike Lurz <eike.lurz@tuta.io>
2025-02-11 20:09:40 +0100

    nvim-lspconfig: get tailwindcss working in phoenix heex

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -156,7 +156,14 @@ return {
         settings = {}
       },
       tailwindcss = {
-        filetypes = { 'heex', 'aspnetcorerazor', 'astro', 'astro-markdown', 'blade', 'clojure', 'django-html', 'htmldjango', 'edge', 'eelixir', 'elixir', 'ejs', 'erb', 'eruby', 'gohtml', 'gohtmltmpl', 'haml', 'handlebars', 'hbs', 'html', 'htmlangular', 'html-eex', 'heex', 'jade', 'leaf', 'liquid', 'markdown', 'mdx', 'mustache', 'njk', 'nunjucks', 'php', 'razor', 'slim', 'twig', 'css', 'less', 'postcss', 'sass', 'scss', 'stylus', 'sugarss', 'javascript', 'javascriptreact', 'reason', 'rescript', 'typescript', 'typescriptreact', 'vue', 'svelte', 'templ' }
+        validate = true,
+        settings = {
+          tailwindCSS = {
+            includeLanguages = {
+              heex = 'html',
+            }
+          }
+        },
       },
       roc_ls = {},
       helmls = {
