38b2b184cf58b480f3d828cc6d2cd704cafee8dc
Eike Lurz <eike.lurz@sportalliance.com>
2025-02-26 08:52:44 +0100

    nvim: add typescript-tools for better vue development

---
+++ nvim/.config/nvim/lua/plugins/typescript-tools.lua
@@ -0,0 +1,31 @@
+return {
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {},
+  ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
+  config = function ()
+    require('typescript-tools').setup {
+      on_attach = function (client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      filetypes = {
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
+        'vue',
+      },
+      settings = {
+        tsserver_plugins = {
+          -- requires: npm i -g @vue/typescript-plugin
+          '@vue/typescript-plugin',
+        },
+        jsx_close_tag = {
+          enable = true,
+          filetypes = { 'javascriptreact', 'typescriptreact' },
+        },
+      },
+    }
+  end,
+}
