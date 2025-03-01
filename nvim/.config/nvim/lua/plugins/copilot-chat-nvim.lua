46e9c5545ba95307021da813ed8d3277b6959148
Eike Lurz <eike.lurz@tuta.io>
2025-03-01 17:22:06 +0100

    nvim: finally set up copilot chat properly

--- nvim/.config/nvim/lua/plugins/copilot-chat-nvim.lua
+++ nvim/.config/nvim/lua/plugins/copilot-chat-nvim.lua
@@ -1,11 +1,29 @@
 return {
   'CopilotC-Nvim/CopilotChat.nvim',
-  branch = 'canary',
   dependencies = {
     { 'zbirenbaum/copilot.lua' },
     { 'nvim-lua/plenary.nvim' },
   },
+  branch = 'main',
+  enabled = true,
+  cmd = { 'CopilotChatOpen', 'CopilotChat' },
   build = 'make tiktoken',
   opts = {
+    -- :CopilotChatPrompts - for selecting prompts
+    -- :CopilotChatModels - for selecting models
+    -- :CopilotChatAgents - for selecting agents
+    -- #<context>:<input> - for selecting context input
+    -- :CopilotChat <input>? 	Open chat with optional input
+    -- :CopilotChatOpen 	Open chat window
+    -- :CopilotChatClose 	Close chat window
+    -- :CopilotChatToggle 	Toggle chat window
+    -- :CopilotChatStop 	Stop current output
+    -- :CopilotChatReset 	Reset chat window
+    -- :CopilotChatSave <name>? 	Save chat history
+    -- :CopilotChatLoad <name>? 	Load chat history
+    -- :CopilotChatPrompts 	View/select prompt templates
+    -- :CopilotChatModels 	View/select available models
+    -- :CopilotChatAgents 	View/select available agents
+    -- :CopilotChat<PromptName> 	Use specific prompt template
   }
 }
