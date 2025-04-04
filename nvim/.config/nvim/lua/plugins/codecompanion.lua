8cda8e89a39e81b3e479d126cba3a0cbbe851113
Eike Lurz <eike.lurz@sportalliance.com>
2025-04-04 08:37:00 +0200

    nvim(codecompanion): setup openai & anthropic

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -10,8 +10,25 @@ return {
     { '<leader>cc', ':CodeCompanionChat Toggle<CR>', desc = 'Toggle CodeCompanionChat' },
     { '<leader>cq', ':CodeCompanion', desc = 'Inline CodeCompanion' },
     { '<leader>cm', ':CodeCompanionCmd<CR>', desc = 'CodeCompanion Commands' },
-    -- :CodeCompanion
+    { '<leader>ca', ':CodeCompanionActions<CR>', desc = 'CodeCompanion Actions' },
   },
   config = true,
-  opts = {},
+  opts = {
+    adapters = {
+      anthropic = function ()
+        return require('codecompanion.adapters').extend('anthropic', {
+          env = {
+            api_key = 'cmd:op read op://employee/zcp7jc5d5byynqojxzbcpwd67y/password --no-newline',
+          },
+        })
+      end,
+      openai = function ()
+        return require('codecompanion.adapters').extend('openai', {
+          env = {
+            api_key = 'cmd:op read op://employee/if556elvlh26lyqb3idfa2kkn4/password --no-newline',
+          },
+        })
+      end,
+    }
+  },
 }
