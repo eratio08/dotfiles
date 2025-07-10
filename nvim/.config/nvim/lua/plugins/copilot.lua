8082a9c344ed2c4afc140225539d69742a390b1e
Eike Lurz <eike.lurz@sportalliance.com>
2025-07-10 13:08:23 +0200

    nvim: add helper to trigger copilot manually

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -3,25 +3,52 @@ return {
   cmd = 'Copilot',
   -- event = 'BufReadPost',
   keys = {
-    { '<M-]>', desc = 'Copilot next' }
+    { '<M-]>', desc = 'Copilot next' },
+    {
+      '<leader>ct',
+      function ()
+        require('copilot.suggestion').toggle_auto_trigger()
+      end,
+      desc = 'Copilot Toggle Auto Suggestions'
+    }
   },
-  opts = {
-    suggestion = {
-      auto_trigger = false,
-      keymap = {
-        accept = '<M-CR>',
-        next = '<M-]>',
-        prev = '<M-[>',
+  config = function ()
+    require('copilot').setup({
+      panel = {
+        enabled = false,
+        keymap = {
+          open = '<M-o>',
+        },
       },
-    },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-      ocaml = false,
-      roc = false,
-      elixir = false,
-    },
-    -- copilot_node_command = '~/.asdf/shims/node',
-  },
+      suggestion = {
+        auto_trigger = false,
+        keymap = {
+          accept = '<M-CR>',
+          next = '<M-]>',
+          prev = '<M-[>',
+        },
+      },
+      filetypes = {
+        markdown = true,
+        help = true,
+        ocaml = false,
+        roc = false,
+        elixir = false,
+      },
+    })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuOpen',
+      callback = function ()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuClose',
+      callback = function ()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
+  end
 }
