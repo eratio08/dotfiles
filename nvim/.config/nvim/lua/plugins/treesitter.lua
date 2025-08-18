ac319aad487db2577d95ee69c2d414a1542fa6a4
Eike Lurz <eike.lurz@sportalliance.com>
2025-08-18 07:43:40 +0200

    treesitter: unpin version

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,12 +2,12 @@ return {
   enabled = true,
   'nvim-treesitter/nvim-treesitter',
   branch = 'main',
-  commit = 'faf63903ffa05a9dadd56258d737a0243393c0f5',
   lazy = false,
   build = ':TSUpdate',
   dependencies = {
-    -- 'nvim-treesitter/nvim-treesitter-textobjects',
+    { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'main' },
     -- 'nvim-treesitter/nvim-treesitter-refactor',
+    'folke/which-key.nvim',
   },
   config = function ()
     local ts = require('nvim-treesitter')
@@ -20,13 +20,14 @@ return {
       callback = function (ev)
         if not vim.list_contains(ts.get_installed(), ev.match) then
           print('Running "TSInstall ' .. ev.match .. '"')
-          ts.install({ ev.match })
+          ts.install({ ev.match }):wait(300000) -- wait max 5min
           print('Installed grammer for "' .. ev.match .. '"')
         end
 
-        vim.treesitter.start()
+        vim.treesitter.start(ev.buf, ev.match)
         vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        vim.bo.syntax = 'on'
       end,
     })
 
@@ -36,5 +37,28 @@ return {
     --     files = { 'src/parser.c', 'src/scanner.c' },
     --   },
     -- }
+
+    require('nvim-treesitter-textobjects').setup {
+      select = {
+        lookahead = true,
+        selection_modes = {
+          ['@parameter.outer'] = 'v', -- charwise
+          ['@function.outer'] = 'V',  -- linewise
+          ['@class.outer'] = '<c-v>', -- blockwise
+        },
+        include_surrounding_whitespace = false,
+      },
+    }
+
+    local to = require('nvim-treesitter-textobjects.select')
+    require('which-key').add({
+      { 'af', function () to.select_textobject('@function.outer', 'textobjects') end, desc = 'Around function', mode = { 'x', 'o' } },
+      { 'if', function () to.select_textobject('@function.inner', 'textobjects') end, desc = 'In function', mode = { 'x', 'o' } },
+      { 'ac', function () to.select_textobject('@class.outer', 'textobjects') end, desc = 'Around class', mode = { 'x', 'o' } },
+      { 'ic', function () to.select_textobject('@class.inner', 'textobjects') end, desc = 'In class', mode = { 'x', 'o' } },
+      { 'as', function () to.select_textobject('@local.scope', 'locals') end, desc = 'Around scope', mode = { 'x', 'o' } },
+      { 'ab', function () to.select_textobject('@block.outer', 'textobjects') end, desc = 'Around block', mode = { 'x', 'o' } },
+      { 'ib', function () to.select_textobject('@block.inner', 'textobjects') end, desc = 'In block', mode = { 'x', 'o' } },
+    })
   end
 }
