29e648b694f1d1f3ceee099d7d5a08ab494682a4
Eike Lurz <eike.lurz@sportalliance.com>
2025-08-18 07:44:12 +0200

    conform-nvim: setup helper for saving without formatting

--- nvim/.config/nvim/lua/plugins/conform-nvim.lua
+++ nvim/.config/nvim/lua/plugins/conform-nvim.lua
@@ -8,7 +8,10 @@ return {
   config = function (_, opts)
     opts = {
       notify_on_error = true,
-      format_on_save = function ()
+      format_on_save = function (bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         return {
           timeout_ms = 60000,
           lsp_format = 'fallback',
@@ -58,5 +61,24 @@ return {
       end
       require('conform').format({ async = true, lsp_fallback = true, range = range })
     end, { range = true })
+
+
+    vim.api.nvim_create_user_command('FormatDisable', function (args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+    vim.api.nvim_create_user_command('FormatEnable', function ()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
   end,
 }
