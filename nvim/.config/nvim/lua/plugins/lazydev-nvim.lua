11955fd5c5f64b80c66449d1b215ee536f879f2a
Eike Lurz <eike.lurz@sportalliance.com>
2025-11-25 13:29:57 +0100

    nvim(lazydev-nvim): cleanup wezterm-types

--- nvim/.config/nvim/lua/plugins/lazydev-nvim.lua
+++ nvim/.config/nvim/lua/plugins/lazydev-nvim.lua
@@ -4,12 +4,10 @@ return {
   ft = 'lua',
   dependencies = {
     'Bilal2453/luvit-meta',
-    'justinsgithub/wezterm-types',
   },
   opts = {
     library = {
       { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      { path = 'wezterm-types', mods = { 'wezterm' } },
       { path = 'plenary.nvim', words = { 'describe', 'it', 'before_each', 'after_each' } },
     },
   },
