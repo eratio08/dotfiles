24c34de69c743158ad5b21c851b8b6254176a825
Eike Lurz <eike.lurz@sportalliance.com>
2026-02-02 09:01:56 +0100

    nvim(toggleterm): Make default width 1/3 again

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -7,19 +7,11 @@ return {
   },
   config = function ()
     require('toggleterm').setup({
-      size = vim.o.columns * 0.5,
+      size = vim.o.columns * 0.33333,
       open_mapping = [[<c-\>]],
       direction = 'vertical',
       persist_size = false,
       highlights = require('rose-pine.plugins.toggleterm'),
-      float_opts = {
-        border = 'curved',
-        winblend = 0,
-        highlights = {
-          border = 'Normal',
-          background = 'Normal',
-        },
-      },
     })
   end
 }
